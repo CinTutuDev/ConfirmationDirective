0287586e2c78d3932d6d47e1cfe6883d9551e406
cinta <cintatafur@gmail.com>
2023-09-21 09:21:16 +0200

    💄 Create(button):hago btn y agrego bootstrap

--- src/app/my-button/my-button.component.ts
+++ src/app/my-button/my-button.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-my-button',
   templateUrl: './my-button.component.html',
-  styleUrls: ['./my-button.component.scss']
+  styleUrls: ['./my-button.component.scss'],
 })
 export class MyButtonComponent {
-
+  save() {
+    console.log('he guardado los datos');
+  }
 }
