2bc7fa537c409f0dab8a1cb7cc6ae7c86424996b
cinta <cintatafur@gmail.com>
2023-09-21 09:37:57 +0200

    ✅ Add(HostListener):decorador e que crea evento DOM para escuchar y dar método cuando ocurre dicho evento

--- src/app/confirmation-panel.directive.ts
+++ src/app/confirmation-panel.directive.ts
@@ -1,10 +1,15 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener } from '@angular/core';
 
 @Directive({
-  selector: '[appConfirmationPanel]'
+  selector: '[appConfirmationPanel]',
 })
 export class ConfirmationPanelDirective {
+ /*  numberOfClicks = 0; */
+  @HostListener('click', ['$event.target'])
+  onClick(btn: any) {
+    return window.confirm(' Estás seguro de querer guardar?');
+    /* console.log('button', btn, 'number of clicks:', this.numberOfClicks++); */
+  }
 
-  constructor() { }
-
+  constructor() {}
 }
