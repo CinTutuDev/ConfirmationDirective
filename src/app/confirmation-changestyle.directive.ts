ed2baf723bedf02088b38357cf25b3326881baa1
cinta <cintatafur@gmail.com>
2023-09-22 10:01:48 +0200

    📌 Comentarios de uso

--- src/app/confirmation-changestyle.directive.ts
+++ src/app/confirmation-changestyle.directive.ts
@@ -1,12 +1,14 @@
 import { Directive, HostListener, ElementRef } from '@angular/core';
-
+/*ElementRef --> permite el acceso directo al DOM  */
 @Directive({
   selector: '[changestyle]',
 })
 export class ConfirmationChangestyleDirective {
+  /*  Evento al pasar el ratón*/
   @HostListener('mouseover') onMouseOver() {
     this.elementRef.nativeElement.style.backgroundColor = '#FFD700';
   }
+  /*  Evento fuera del ratón*/
   @HostListener('mouseout') onMouseOut() {
     this.elementRef.nativeElement.style.backgroundColor = '';
   }
